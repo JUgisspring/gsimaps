b370349293f901a418ac819c01b07bfea96dc36b
JUgisspring <37054449+JUgisspring@users.noreply.github.com>
2018-03-05 11:21:46 +0900

    Update gsimaps.js

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -28,8 +28,9 @@ CONFIG.layersTab = [
 			'./layers_txt/layers_topic.txt',
 			'./layers_txt/layers_skhb.txt',
 			'./layers_txt/layers_topic_kusatsushirane.txt',
-			'./layers_txt/layers_topic_nishi.txt'
-					]
+			'./layers_txt/layers_topic_nishi.txt',
+			'./layers_txt/layers_topic_sample2.txt'
+							]
 	},
 	{
 		'caption' : 'ベースマップ',
